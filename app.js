1bba929d35c2c3bf03a512145bf00da68f404272
theylonsoares25123 <theylon.silva@rcell.com.br>
2024-08-07 17:48:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const supportersCount = document.getElementById('supporters-count');
-    const loadingIcon = document.getElementById('loading-icon');
-
-    // URL da API do SheetMonkey
     const apiURL = 'https://api.sheetmonkey.io/form/9uiwfDSMKPWwF6ZqTA2dpM';
 
-    // Função para buscar dados da planilha
     const fetchSheetData = async () => {
         try {
             const response = await fetch(apiURL);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
             const data = await response.json();
-            return data[0]['Apoiadores']; // Substitua 'Apoiadores' pelo nome da coluna que contém o valor desejado
+            return data[0]['Apoiadores']; // Substitua 'Apoiadores' pelo nome da coluna correta
         } catch (error) {
             console.error('Erro ao buscar dados da planilha:', error);
+            return 'Erro ao carregar';
         }
     };
 
-    // Atualizar a contagem inicial de apoiadores
     const updateSupportersCount = async () => {
-        loadingIcon.style.display = 'inline-block'; // Mostrar ícone de carregamento
         const count = await fetchSheetData();
-        supportersCount.textContent = `( ${count} Pessoas )`;
-        loadingIcon.style.display = 'none'; // Ocultar ícone de carregamento
+        supportersCount.textContent = `(${count} Pessoas)`;
     };
 
-    // Chama a função para atualizar a contagem inicial
     updateSupportersCount();
 });
